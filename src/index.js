2bafb87d1b370107cc806086aa60d19aa29cf366
Blazer <sstubbs012@gmail.com>
2019-02-26 21:20:09 -0500

    added errorboundary and first hook

--- src/index.js
+++ src/index.js
@@ -19,32 +19,67 @@ function PokemonListItem({ className, component: Component = "li", ...props }) {
   );
 }
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback ? (
+        this.props.fallback
+      ) : (
+        <div>Something went wrong</div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
 // 5. pull your resource-reading UI into a component
-function PokemonList() {
+function PokemonList({ onSelect }) {
   return (
     <ul>
       {/* 6. read resource data into and from the cache */}
       {PokemonCollectionResource.read().results.map(pokemon => (
-        <PokemonListItem key={pokemon.name}>{pokemon.name}</PokemonListItem>
+        <PokemonListItem 
+          onClick={() => onSelect(pokemon.url.split('/')[6])} 
+          key={pokemon.name}>{pokemon.name}</PokemonListItem>
       ))}
     </ul>
   );
 }
 
 function App() {
+  let [selectedPokemonId, setSelectedPokemonId] = React.useState(1);
+
   return (
     <div>
       <h1>
         <span role="img" aria-label="React Holiday">
           ⚛️🎄✌️
         </span>
-        : Day 3
+        : Day 10
       </h1>
-      {/* 7. wrap your resource-reading component in the Suspense component */}
-      {/*    ...and provide a fallback */}
-      <React.Suspense fallback={<div>...loading</div>}>
-        <PokemonList />
-      </React.Suspense>
+      
+      <strong>selected pokemon id: {selectedPokemonId}</strong>
+      <ErrorBoundary fallback={<div>OOps! Pokemon got away</div>}>
+        {/* 7. wrap your resource-reading component in the Suspense component */}
+        {/*    ...and provide a fallback */}
+        <React.Suspense fallback={<div>...loading</div>}>
+          <PokemonList onSelect={id => setSelectedPokemonId(id)}/>
+        </React.Suspense>
+      </ErrorBoundary>
     </div>
   );
 }
